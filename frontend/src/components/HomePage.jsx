6fa61736477e518c182f5eb35e5eff29ae01cfb1
Tafadzwa Munyuki <35223331+tafadzwamunyuki@users.noreply.github.com>
2025-04-21 00:28:47 +0100

    Update HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,13 +5,13 @@ import '../index.css';
 export default function HomePage() {
   return (
     <div className='text-center'>
-      <h1 className='note'>PLEASE DISABLE VPN IN ORDER TO ACCESS ARTICLES!</h1>
-    <div className="container text-center mt-5">
+      <h1 className='note'>DISABLE VPN TO ACCESS ARTICLES!</h1>
+    <div className="container text-center mt-1">
       <h1 className="mb-4">📝 Freelog: The hub where creativity meets</h1>
       <p className="lead">Online space fostering collaboration, empowering creativity, and diversity </p>
-      <Link to="/blogs" className="btn btn-primary mt-3">Go to Articles</Link>
+      <Link to="/blogs" className="btn btn-primary mt-8">Go to Articles</Link>
       <img src="/brain.jpg"
-        className='landing-img img-fluid rounded shadow-lg mb-4'
+        className='landing-img img-fluid shadow-lg mb-4'
         alt='' />
     </div>
     </div>
